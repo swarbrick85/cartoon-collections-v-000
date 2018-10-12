6fc8917d131bf1daf8a151ff31e782bd704d36b9
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-12 18:20:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  x = 0 
+  while x < names.size 
+  names.map do |name| 
+    y = "#{x+1}. #{name}"
+    puts y 
+    x += 1
+  end
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(eers)
+  x = 0 
+  y = []
+  while x < eers.size 
+  eers.each do |eer| 
+    y << "#{eer.capitalize}!"
+    x += 1
+  end
+end
+return y
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+  if call.size > 4 
+    return true 
+  end 
+end 
+return false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
